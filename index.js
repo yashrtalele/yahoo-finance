7105274441bf7cb316517de01d6d0d228af45b33
Yash Talele <yashrtalele@gmail.com>
2024-11-09 11:54:38 +0530

    Added history at the listen part

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const { initialData } = require("./cron/initialData");
+const { history } = require("./utils/history");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,12 @@ app.get("/", (req, res) => {
 });
 app.use("/api", routes);
 initialData();
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+app.listen(
+  PORT,
+  history(
+    `https://finance.yahoo.com/quote/AEDINR=X/history/?period1=2024-11-02&period2=2024-11-09`,
+  ),
+  () => {
+    console.log(`Server started on port ${PORT}`);
+  },
+);
